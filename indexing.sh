34a1b5357ea4bced9469d7278bb1de271f781ad6
Dongseok Han <xcrossx@gmail.com>
2019-01-22 00:36:22 +0900

    Remove symlink to hardlink

--- indexing.sh
+++ indexing.sh
@@ -1,1 +1,11 @@
-/home/ds/indexing.sh
+#!/bin/bash
+
+#ctags
+ctags -R --exclude=output
+
+#cscope
+rm -rf cscope.out cscope.files
+find . \( -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' -o -name '*.s'\
+  -o -name '*.S' -o -name '*.hpp' \) -print > cscope.files
+
+echo "" | cscope -i cscope.files
